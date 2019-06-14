9355007cb5a75b515dd2af3517b8eaf4ee54bd7f
liuxy <938651469@qq.com>
2019-06-14 15:08:17 +0800

    add egg-scripts and egg-mock

---
+++ test/index.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mm = require('egg-mock');
+
+describe('example helloworld test', () => {
+  let app;
+
+  before(() => {
+    app = mm.app();
+    return app.ready();
+  });
+
+  after(() => app.close());
+
+  it('should GET / 200', () => {
+    return app.httpRequest()
+      .get('/')
+      .expect(200)
+      .expect('Hello World');
+  });
+});
